ef0427ab78623e3d95b11f30d94886a710a78f88
Nivetha <taquanga6dp@gmail.com>
2025-08-09 21:41:54 +0900

    config response after call api user interceptor axios

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,6 +1,6 @@
-import { Input, Button } from "antd";
+import { Input, Button, notification } from "antd";
 import { useState } from "react";
-import axios from "axios";
+import { createUserAPI } from "../../services/api.service";
 
 const UserForm = () => {
     const [fullName, setFullName] = useState("");
@@ -8,17 +8,17 @@ const UserForm = () => {
     const [password, setPassword] = useState("");
     const [phone, setPhone] = useState("");
 
-    const handleClickBtn = () => {
-        const URL_BACKEND = "http://localhost:8080/api/v1/user";
-        const data = {
-            fullName: fullName,
-            email: email,
-            password: password,
-            phone: phone,
-        };
+    const handleClickBtn = async () => {
+        const res = await createUserAPI(fullName, email, password, phone);
+        if (res?.data) {
+            notification.success({
+                message: "create user",
+                description: "tao user thanh cong",
+            });
+            console.log(res);
+        }
 
-        axios.post(URL_BACKEND, data);
-        console.log(">>>check state:", { fullName, email, password, phone });
+        // console.log(">>>check state:", { fullName, email, password, phone });
     };
     return (
         <div className="user-form" style={{ margin: "20px " }}>
