a8eec352bdc12ed34d6f2f3c8a0ccd1e478bb5e1
Nivetha <taquanga6dp@gmail.com>
2025-08-18 22:13:51 +0900

    add popup modal to create user

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,4 +1,4 @@
-import { Input, Button, notification } from "antd";
+import { Input, Button, notification, Modal } from "antd";
 import { useState } from "react";
 import { createUserAPI } from "../../services/api.service";
 
@@ -7,64 +7,85 @@ const UserForm = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [phone, setPhone] = useState("");
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const handleClickBtn = async () => {
+    const handleSubmitBtn = async () => {
         const res = await createUserAPI(fullName, email, password, phone);
         if (res?.data) {
             notification.success({
                 message: "create user",
                 description: "tao user thanh cong",
             });
+            setIsModalOpen(false);
             console.log(res);
+        } else {
+            notification.error({
+                message: "error create user",
+                description: JSON.stringify(res.message),
+            });
         }
-
-        // console.log(">>>check state:", { fullName, email, password, phone });
+        console.log(">>>check state:", { fullName, email, password, phone });
     };
     return (
         <div className="user-form" style={{ margin: "20px " }}>
-            <div style={{ display: "flex", gap: "20px", flexDirection: "column" }}>
-                <div>
-                    <span>FullName</span>
-                    <Input
-                        value={fullName}
-                        onChange={(val) => {
-                            setFullName(val.target.value);
-                        }}
-                    />
-                </div>
-                <div>
-                    <span>Email</span>
-                    <Input
-                        value={email}
-                        onChange={(val) => {
-                            setEmail(val.target.value);
-                        }}
-                    />
-                </div>
-                <div>
-                    <span>Password</span>
-                    <Input.Password
-                        value={password}
-                        onChange={(val) => {
-                            setPassword(val.target.value);
-                        }}
-                    />
-                </div>
-                <div>
-                    <span>Phone Number</span>
-                    <Input
-                        value={phone}
-                        onChange={(val) => {
-                            setPhone(val.target.value);
-                        }}
-                    />
-                </div>
-                <div>
-                    <Button type="primary" onClick={handleClickBtn}>
-                        Create User
-                    </Button>
-                </div>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <h3>Table Users</h3>
+                <Button type="primary" onClick={() => setIsModalOpen(true)}>
+                    Create User
+                </Button>
             </div>
+
+            <Modal
+                title="Basic Modal"
+                closable={{ "aria-label": "Custom Close Button" }}
+                open={isModalOpen}
+                onOk={() => {
+                    handleSubmitBtn();
+                }}
+                onCancel={() => {
+                    setIsModalOpen(false);
+                }}
+                maskClosable={false}
+                okText={"CREATE"}>
+                <div style={{ display: "flex", gap: "20px", flexDirection: "column" }}>
+                    <div>
+                        <span>FullName</span>
+                        <Input
+                            value={fullName}
+                            onChange={(val) => {
+                                setFullName(val.target.value);
+                            }}
+                        />
+                    </div>
+                    <div>
+                        <span>Email</span>
+                        <Input
+                            value={email}
+                            onChange={(val) => {
+                                setEmail(val.target.value);
+                            }}
+                        />
+                    </div>
+                    <div>
+                        <span>Password</span>
+                        <Input.Password
+                            value={password}
+                            onChange={(val) => {
+                                setPassword(val.target.value);
+                            }}
+                        />
+                    </div>
+                    <div>
+                        <span>Phone Number</span>
+                        <Input
+                            value={phone}
+                            onChange={(val) => {
+                                setPhone(val.target.value);
+                            }}
+                        />
+                    </div>
+                </div>
+            </Modal>
         </div>
     );
 };
--- src/services/axios.customize.js
+++ src/services/axios.customize.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8080/",
+    baseURL: import.meta.env.VITE_BACKEND_URL,
 });
 
 // Add a request interceptor
@@ -22,12 +22,14 @@ instance.interceptors.response.use(
         // Any status code that lie within the range of 2xx cause this function to trigger
         // Do something with response data
 
-        if (response.data.data) return response.data;
+        if (response.data && response.data.data) return response.data;
         return response;
     },
     function (error) {
         // Any status codes that falls outside the range of 2xx cause this function to trigger
         // Do something with response error
+
+        if (error.response && error.response.data) return error.response.data;
         return Promise.reject(error);
     }
 );
