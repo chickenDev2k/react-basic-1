8eeaae0c21d292dc42150e6b808e9ac3bf841b0a
Nivetha <taquanga6dp@gmail.com>
2025-08-27 21:24:41 +0900

    feat form register added

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,5 +1,36 @@
+import { Button, Form, Input } from "antd";
+
 const RegisterPage = () => {
-    return <div>register page</div>;
+    const [form] = Form.useForm();
+    const onFinish = (value) => {
+        console.log(value);
+    };
+    return (
+        <div style={{ padding: "40px" }}>
+            <Form layout="vertical" form={form} onFinish={onFinish}>
+                <Form.Item label="Full Name" name="fullName" rules={[{ required: true }]}>
+                    <Input />
+                </Form.Item>
+
+                <Form.Item label="Email" name="email" rules={[{ required: true }]}>
+                    <Input />
+                </Form.Item>
+
+                <Form.Item label="Password" name="password">
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item label="Phone Number" name="phone">
+                    <Input />
+                </Form.Item>
+
+                <Form.Item label="">
+                    <Button type="primary" htmlType="submit" onClick={() => {}}>
+                        Register
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
+    );
 };
 
 export default RegisterPage;
