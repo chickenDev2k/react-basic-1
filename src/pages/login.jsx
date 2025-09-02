963aa6909fff7213b2e3089b59ac33908ae5a6b7
Nivetha <taquanga6dp@gmail.com>
2025-09-02 21:41:47 +0900

    feat: completed login module

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,75 +1,60 @@
-import { Form, notification, Row, Col, Input, Button, Divider } from "antd";
+import { Form, notification, Row, Col, Input, Button, Divider, message } from "antd";
 import { Link, useNavigate } from "react-router-dom";
 import { loginUserAPI } from "../services/api.service";
+import { useState } from "react";
 
 const LoginPage = () => {
     const [form] = Form.useForm();
+    const [isLoading, setIsLoading] = useState(false);
     let navigate = useNavigate();
 
     const onFinish = async (values) => {
         console.log(values);
-        //call api
-        const res = await loginUserAPI(
-            values.fullName,
-            values.email,
-            values.password,
-            values.phone
-        );
+        //call login api
+
+        setIsLoading(true);
+        const res = await loginUserAPI(values.email, values.password);
         if (res.data) {
-            notification.success({
-                message: "Register User",
-                description: "Dang ki user thanh cong ",
+            message.success({
+                message: "Login User",
+                description: "Dang nhap thanh cong ",
             });
-            navigate("/login");
+            navigate("/");
         } else {
             notification.error({
-                message: "Register User error",
+                message: "Login User error",
                 description: JSON.stringify(res.message),
             });
         }
+        setIsLoading(false);
     };
 
     return (
         <Row justify={"center"} style={{ margin: "40px auto" }}>
-            <Col xs={24} md={18} lg={12}>
-                <p style={{ textAlign: "center" }}>Dang nhap</p>
+            <Col xs={24} md={18} lg={6}>
+                <p style={{ textAlign: "center", fontSize: "3em" }}>Login</p>
+
                 <Form layout="vertical" form={form} onFinish={onFinish}>
-                    <Row justify={"center"}>
-                        <Col xs={24} md={8}>
-                            <Form.Item
-                                label="Email"
-                                name="email"
-                                rules={[{ required: true, message: "input this field" }]}>
-                                <Input />
-                            </Form.Item>
-                        </Col>
-                    </Row>
-
-                    <Row justify={"center"}>
-                        <Col xs={24} md={8}>
-                            <Form.Item label="Password" name="password">
-                                <Input.Password />
-                            </Form.Item>
-                        </Col>
-                    </Row>
-
-                    <Row justify={"center"}>
-                        <Col xs={12} md={4}>
-                            <Button type="primary" htmlType="submit">
-                                Login
-                            </Button>
-                        </Col>
-                        <Col xs={12} md={4}>
-                            <Link to={"/"}>Go to homepage -- </Link>
-                        </Col>
-                    </Row>
-                    <Divider />
-                    <Row justify={"center"} style={{ marginTop: "30px" }}>
-                        <Col xs={12} md={6}>
-                            Chua co tai khoan? <Link to={"/register"}>Dang ki tai day</Link>
-                        </Col>
-                    </Row>
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[{ required: true, message: "input this field" }]}>
+                        <Input />
+                    </Form.Item>
+                    <Form.Item label="Password" name="password">
+                        <Input.Password />
+                    </Form.Item>
                 </Form>
+                <Row justify={"space-between"}>
+                    <Button loading={isLoading} type="primary" onClick={() => form.submit()}>
+                        Login
+                    </Button>
+                    <Link to={"/"}>Go to homepage</Link>
+                </Row>
+                <Divider />
+                <Row justify={"center"}>
+                    <Link to={"/register"}>Chua co tai khoan? Dang ki tai khoan </Link>
+                </Row>
             </Col>
         </Row>
     );
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -23,7 +23,15 @@ const registerUserAPI = (fullName, email, password, phone) => {
     return axios.post(URL_BACKEND, data);
 };
 
-const loginUserAPI = () => {};
+const loginUserAPI = (email, password) => {
+    const URL_BACKEND = "/api/v1/auth/login";
+    const data = {
+        username: email,
+        password: password,
+        delay: 5000,
+    };
+    return axios.post(URL_BACKEND, data);
+};
 const updateUserAPI = (_id, fullName, phone) => {
     const URL_BACKEND = "/api/v1/user";
     const data = {
